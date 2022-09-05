3ee798025783d7ece613c9cdaebbf1ba4952a619
Vin Howe <24789592+vinhowe@users.noreply.github.com>
2022-09-05 17:54:03 -0600

    Black format

--- PythonIntro/python_intro.py
+++ PythonIntro/python_intro.py
@@ -12,25 +12,25 @@ if __name__ == "__main__":
 
 # Problem 2
 def sphere_volume(r):
-    """ Return the volume of the sphere of radius 'r'.
+    """Return the volume of the sphere of radius 'r'.
     Use 3.14159 for pi in your computation.
     """
     PI = 3.14159
-    return (4/3)* PI * (r ** 3)
+    return (4 / 3) * PI * (r**3)
 
 
 # Problem 3
 def isolate(a, b, c, d, e):
-    """ Print the arguments separated by spaces, but print 5 spaces on either
+    """Print the arguments separated by spaces, but print 5 spaces on either
     side of b.
     """
-    print(a, b, c, end=" ", sep=" "*5)
+    print(a, b, c, end=" ", sep=" " * 5)
     print(d, e)
 
 
 # Problem 4
 def first_half(my_string):
-    """ Return the first half of the string 'my_string'. Exclude the
+    """Return the first half of the string 'my_string'. Exclude the
     middle character if there are an odd number of characters.
 
     Examples:
@@ -39,10 +39,11 @@ def first_half(my_string):
         >>> first_half("ipython")
         'ipy'
     """
-    return my_string[:len(my_string)//2]
+    return my_string[: len(my_string) // 2]
+
 
 def backward(my_string):
-    """ Return the reverse of the string 'my_string'.
+    """Return the reverse of the string 'my_string'.
 
     Examples:
         >>> backward("python")
@@ -55,7 +56,7 @@ def backward(my_string):
 
 # Problem 5
 def list_ops():
-    """ Define a list with the entries "bear", "ant", "cat", and "dog".
+    """Define a list with the entries "bear", "ant", "cat", and "dog".
     Perform the following operations on the list:
         - Append "eagle".
         - Replace the entry at index 2 with "fox".
@@ -87,7 +88,7 @@ def list_ops():
 
 # Problem 6
 def pig_latin(word):
-    """ Translate the string 'word' into Pig Latin, and return the new word.
+    """Translate the string 'word' into Pig Latin, and return the new word.
 
     Examples:
         >>> pig_latin("apple")
@@ -103,9 +104,10 @@ def pig_latin(word):
 
 # Problem 7
 def palindrome():
-    """ Find and return the largest panindromic number made from the product
+    """Find and return the largest panindromic number made from the product
     of two 3-digit numbers.
     """
+
     def is_palindromic(n):
         str_n = str(n)
         return str_n == str_n[::-1]
@@ -114,7 +116,7 @@ def palindrome():
     largest_operands = None
     for i in range(100, 1000):
         for j in range(100, 1000):
-            product = i*j
+            product = i * j
             if is_palindromic(product):
                 largest = product
                 largest_operands = (i, j)
@@ -123,7 +125,7 @@ def palindrome():
 
 # Problem 8
 def alt_harmonic(n):
-    """ Return the partial sum of the first n terms of the alternating
+    """Return the partial sum of the first n terms of the alternating
     harmonic series, which approximates ln(2).
     """
-    return sum(((-1)**(i + 1)) / i for i in range(1, n))
+    return sum(((-1) ** (i + 1)) / i for i in range(1, n))
